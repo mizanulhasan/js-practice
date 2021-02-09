fdea0edab616e02e990f96736570f328e4570fef
mizanulhasan <mailto.mizanul@gmail.com>
2021-02-09 22:14:35 +0600

    Wokring on function

--- function.js
+++ function.js
@@ -7,9 +7,15 @@
 // var total = first + second;
 // // console.log(total);
 
-function add(num1, num2){
-    var result = num1 + num2;
-    return result;
+// function add(num1, num2){
+//     var result = num1 + num2;
+//     return result;
+// }
+// var sum = add(15, 17);
+// console.log(sum);
+var marks = [89, 97, 89, 68, 94, 86, 79];
+
+for (var i = 0; i <marks.length; i++){
+    var element = marks[i];
+    console.log(element);
 }
-var sum = add(15, 17);
-console.log(sum);
